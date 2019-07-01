55e4d2480bc3575fa52a59ba6366203453198eb2
Ronika <rconner92@gmail.com>
2019-07-01 15:14:12 -0400

    First commit

---
+++ Assets/ChangeName.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            TextMeshProUGUI textmeshPro = GetComponent<TextMeshProUGUI>();
+            textmeshPro.SetText("Hello World! My name is Ronika Conner");
+        }
+    }
+}
